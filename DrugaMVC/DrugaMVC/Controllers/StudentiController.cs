0c77050c1f4510ab4bbc428020b8ed7310c1f7d1
Barbara Pušnar <barbara.pusnar@scng.si>
2023-01-19 19:00:41 +0100

    Nova MVC

---
+++ DrugaMVC/DrugaMVC/Controllers/StudentiController.cs
@@ -0,0 +1,28 @@
+﻿using DrugaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrugaMVC.Controllers
+{
+    public class StudentiController : Controller
+    {
+        // GET: Studenti
+        public ActionResult Index()
+        {
+           var studentList = new List<Student>{
+           new Student() { Id = 1, Ime = "John", Starost = 18 } ,
+           new Student() { Id = 2, Ime = "Steve", Starost = 21 } ,
+           new Student() { Id = 3, Ime = "Bill", Starost = 25 } ,
+           new Student() { Id = 4, Ime = "Ram" , Starost = 20 } ,
+           new Student() { Id = 5, Ime = "Ron" , Starost = 31 } ,
+           new Student() { Id = 4, Ime = "Chris" , Starost = 17 } ,
+           new Student() { Id = 4, Ime = "Rob" , Starost = 19 }
+         };
+
+            return View(studentList);
+        }
+    }
+}
---
+++ DrugaMVC/DrugaMVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace DrugaMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string  Ime { get; set; }
+        public int Starost { get; set; }
+    }
+}
